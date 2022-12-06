b4135b0c8ea3b8dd2b50fadbe27273eb0a24c10b
kyle32288 <kyle@weavs.us>
2022-12-06 21:07:44 +0100

    added constants to set height and width of grid

--- grid.js
+++ grid.js
@@ -1,13 +1,19 @@
-let myHTML = ''
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'white', 'black'];
+const gridHeight = 16;
+const gridWidth = 40;
+let myHTML = ''
 let myColor;
 function addColor(c){
     document.querySelector('#colors').innerHTML += 
         `<div class="color" id="${c}" style="background-color:${c}"></div>`
 }
-for (let i=0; i < 16; i++){
+function pickColor(color){
+    myColor = color;
+    
+}
+for (let i=0; i < gridHeight; i++){
     myHTML += `<div class="row" id="row${i}">`;
-    for (let j = 0; j<16; j++){
+    for (let j = 0; j < gridWidth; j++){
         myHTML += `<div class="box" id="row${i}-box${j}"></div>`;
     }
     myHTML += '</div>'
