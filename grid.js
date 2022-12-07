7abd1b88117b50af85c8824cdd144d869457b515
kyle32288 <kyle@weavs.us>
2022-12-07 12:38:31 +0100

    Added keys to cycle colors forwards and backwards, as well as a key to set color to none

--- grid.js
+++ grid.js
@@ -1,20 +1,40 @@
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'white', 'black'];
-const gridHeight = 16;
-const gridWidth = 40;
+const gridHeight = 70;
+const gridWidth = 120;
 let myHTML = ''
-let myColor;
-function addColor(c){
+let myColor = '';
+function addColor(color, index){
     document.querySelector('#colors').innerHTML += 
-        `<div class="color" id="${c}" style="background-color:${c}"></div>`
+        `<div class="color" data-index="${index}" id="${color}" style="background-color:${color}"></div>`
 }
 function pickColor(color){
     myColor = color;
     document.querySelectorAll('.color').forEach(c => c.classList.remove('selected'));
-    document.querySelector(`#${myColor}`).classList.add('selected');
+    if (myColor){
+        document.querySelector(`#${myColor}`).classList.add('selected');
+    }
 }
 function reset(e){
     if (e.code == 'KeyC'){
         boxes.forEach(box => box.style.backgroundColor = 'white')
+    }
+}
+function cycleColor(e){
+    if (e.code == 'KeyD' || e.code == 'KeyA'){
+        if (myColor == '') currentColorIndex = -1
+        else currentColorIndex = document.querySelector(`#${myColor}`).getAttribute('data-index')
+        let newIndex 
+        if (e.code == 'KeyD') newIndex = +currentColorIndex+1;
+        else newIndex = +currentColorIndex-1;
+        if (newIndex > colors.length - 1) newIndex = 0;
+        if (newIndex < 0) newIndex = (colors.length - 1);
+        let newColor = document.querySelector(`.color[data-index="${newIndex}"]`).id;
+        pickColor(newColor);
+    }
+    if (e.code == 'KeyS'){
+        console.log(e)
+        pickColor('')
+        console.log(myColor)
     }
 }
 for (let i=0; i < gridHeight; i++){
@@ -26,7 +46,7 @@ for (let i=0; i < gridHeight; i++){
 }
 document.querySelector('#grid').innerHTML = myHTML;
 const boxes = document.querySelectorAll('.box')
-colors.forEach(color => addColor(color))
+colors.forEach((color, index) => addColor(color,index))
 colors.forEach(color => document.querySelector(`#${color}`).addEventListener('click', e => pickColor(e.target.id)))
-boxes.forEach(box => box.addEventListener('mouseover', e => e.target.style.backgroundColor = myColor))
-document.addEventListener('keydown', e => reset(e))
+boxes.forEach(box => box.addEventListener('mouseover', e => {if (myColor) e.target.style.backgroundColor = myColor}))
+document.addEventListener('keydown', e => (reset(e), cycleColor(e)))
